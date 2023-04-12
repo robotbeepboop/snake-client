9ebac4ec90af94327652e35c68b7d51f78fe6909
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-12 16:02:32 -0700

    redundant code removed

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const { Server } = require("http");
-const net = require("net");
-const client = require('./client');
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-
-setupInput();
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-  conn.on('data', (data) => {
-    console.log(data)
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+setupInput(connect());
