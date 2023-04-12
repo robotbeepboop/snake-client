38336a47fd248e9802a06e000ac49aebb14dad38
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-12 16:03:10 -0700

    cleaned up code for submission

--- client.js
+++ client.js
@@ -1,29 +1,20 @@
-/*
-Usable commands:
-"Move: up" - move up one square (unless facing down)
-"Move: down" - move down one square (unless facing up)
-"Move: left" - move left one square (unless facing right)
-"Move: right" - move left one square (unless facing left)
-***no 180s allowed
-*/
-
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
+    host: IP,
+    port: PORT
   });
 
   conn.on('connection', () => {
     console.log('Connected! Congrats buddy!');
-    //input name upon connection
     //set name, three letters, format Name: ---
     conn.write('Name: CEB');
   });
 
   conn.on('data', (data) => {
-    console.log(data);
+    console.log('Server says: ', data);
   });
     
   // interpret incoming data as text
