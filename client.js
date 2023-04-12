b1eccbcbc524f441f722c6fb7e7df59690af3101
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-12 12:52:09 -0700

    added event handling

--- client.js
+++ client.js
@@ -1,19 +1,28 @@
 const net = require("net");
 
-const clientConnect = {
-  connect: function () {
-    const conn = net.createConnection({
-      host: 'localhost',
-      port: 50541,
-    });
-    conn.on('data', (data) => {
-      console.log(data)
-    });
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-  
-    return conn;
-  }
-}
 
-module.exports = clientConnect;
+connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  conn.on('connection', () => {
+    console.log('Connected! Congrats buddy!');
+    //input name upon connection
+    //set name, three letters, format Name: ---
+    conn.write('Name: CEB');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+    
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+
+};
+
+module.exports = connect;
