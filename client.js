2c1bf8d87c35bc691983144a5c320b0445f06faf
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-12 13:01:49 -0700

    small changes + notes

--- client.js
+++ client.js
@@ -1,5 +1,13 @@
-const net = require("net");
+/*
+Usable commands:
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+***no 180s allowed
+*/
 
+const net = require("net");
 
 connect = function () {
   const conn = net.createConnection({
@@ -15,7 +23,7 @@ connect = function () {
   });
 
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    console.log(data);
   });
     
   // interpret incoming data as text
